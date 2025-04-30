ec3a380f842d2ce0a7d98b649ea227e66c58ac1e
kiry <kongchankiry2002@gmail.com>
2025-04-30 20:52:10 +0700

    working on venue for part hero section, sport buttom, overview, review

--- src/views/User/Venue.vue
+++ src/views/User/Venue.vue
@@ -1,5 +1,10 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue' // Ensure the path matches your project structure
+import Navbar from '@/components/Navbar.vue';
+import VenueHeroSection from '@/components/venue/VenueHeroSection.vue';
+import SportButtoms from '@/components/venue/SportButtoms.vue';
+import SportOverView from '@/components/venue/SportOverView.vue';
+import SportReview from '@/components/venue/SportReview.vue';
+
 </script>
 <script>
 export default {
@@ -10,6 +15,9 @@ export default {
 <template>
   <main>
     <Navbar /> 
-    <div class="text-center mt-24 text-2xl">Vanue page</div>
+    <VenueHeroSection /> 
+    <SportButtoms /> 
+    <SportOverView /> 
+    <SportReview /> 
   </main>
 </template>
