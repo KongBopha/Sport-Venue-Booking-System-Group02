601d8a0fecb260f0f2631ee47d20dd8c10c8df16
kongbopha <kongbophaa@gmail.com>
2025-06-22 18:29:35 +0700

    user buy equipment

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -1,10 +1,24 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, //  http://localhost:1000/api
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
-export default axiosInstance;
+http.interceptors.request.use((config) => {
+  // Only attach token if it's not a public route
+  if (!config.url.includes('/public/')) {
+    const token = localStorage.getItem('Token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+
+
+export default http;
